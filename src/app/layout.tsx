9899e594e4a059179303c1bfbf6e89b20c4927ba
ByeongGyu An <an622911@gmail.com>
2024-10-07 14:12:00 +0900

    feat: Vercel Web Analytics, Speed insights 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import { Metadata } from 'next'
 import localFont from 'next/font/local'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
+
 import siteConfig from '@root/site.config'
 import { ReactNode } from 'react'
 import './globals.css'
@@ -29,6 +32,8 @@ export default function RootLayout({ children }: LayoutProps) {
         <div className="white dark:from-gray-900 dark:to-gray-800 text-gray-800 dark:text-gray-200">
           {children}
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
