f8ca52ba3d8d94c99c59d05971418ce92ee0abd2
ByeongGyu An <an622911@gmail.com>
2024-10-07 11:15:16 +0900

    chore: 환경변수 설정을 위한 입력방식 변경

--- site.config.ts
+++ site.config.ts
@@ -1,12 +1,13 @@
 import { siteConfig } from '@/lib/siteConfig'
+import { root } from 'postcss'
 
 const config = siteConfig({
-  rootNotionPageId: '',
-  domain: '',
-  title: '',
-  description: '',
-  mainPageTitle: '',
-  homeLinkTitle: 'blog',
+  rootNotionPageId: process.env.ROOT_NOTION_PAGE || '',
+  domain: process.env.DOMAIN || '',
+  title: process.env.TITLE || '',
+  description: process.env.DESCRIPTION || '',
+  mainPageTitle: process.env.NEXT_PUBLIC_MAIN_PAGE_TITLE || '',
+  homeLinkTitle: process.env.NEXT_PUBLIC_HOME_LINK_TITLE || '',
 })
 
 export default config
